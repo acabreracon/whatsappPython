5fcc8645f64be54574aeefcf173c60a780f417b9
Alejandro Cabrera <alejandro.cabrera@beyondexperience.com>
2025-02-15 00:44:09 -0500

    gitignore

--- geminigpt.py
+++ geminigpt.py
@@ -8,7 +8,6 @@ client = genai.Client(api_key= os.getenv("GEMINI_API_KEY"))
 
 def GetResponse(text):
     try:
-
         response = client.models.generate_content(
             model="gemini-2.0-flash",
             contents=text,

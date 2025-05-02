894ffccfc4df283e8af878fb12abd67ff95e1e82
Alejandro Cabrera <alejandro.cabrera@beyondexperience.com>
2025-05-02 10:30:51 -0500

    Se prueba el sistema RAG

--- asistenteVirtualRAG.py
+++ asistenteVirtualRAG.py
@@ -11,73 +11,77 @@ from langchain.schema.runnable import RunnableMap
 from langchain.prompts import ChatPromptTemplate
 
 def asistenteVirtualRag(text):
+    try:
+        reader = PdfReader("Beyond_Experience.pdf")
 
-    reader = PdfReader("Beyond_Experience.pdf")
+        pdf_text = [page.extract_text() for page in reader.pages if page.extract_text()]
+        pdf_text  = [text for text in pdf_text if text]
+        full_text = "\n\n".join(pdf_text)
 
-    pdf_text = [page.extract_text() for page in reader.pages if page.extract_text()]
-    pdf_text  = [text for text in pdf_text if text]
-    full_text = "\n\n".join(pdf_text)
+        print(full_text[:1000])  # Print the first 1000 characters of the PDF text
 
-    print(full_text[:1000])  # Print the first 1000 characters of the PDF text
+        splitter = RecursiveCharacterTextSplitter(
+            chunk_size=500,  # Number of characters per chunk
+            chunk_overlap=50,  # Number of overlapping characters between chunks
+            separators=["\n\n", "\n", ". ", " ", ""],  # Function to calculate the length of a chunk
+        )
+        chunks = splitter.split_text(full_text)
+        print(f"Total de fragmentos: {len(chunks)}")  # Print the number of chunks created
 
-    splitter = RecursiveCharacterTextSplitter(
-        chunk_size=500,  # Number of characters per chunk
-        chunk_overlap=50,  # Number of overlapping characters between chunks
-        separators=["\n\n", "\n", ". ", " ", ""],  # Function to calculate the length of a chunk
-    )
-    chunks = splitter.split_text(full_text)
-    print(f"Total de fragmentos: {len(chunks)}")  # Print the number of chunks created
+        if "GOOGLE_API_KEY" not in os.environ:
+            os.environ["GOOGLE_API_KEY"] = "AIzaSyCrulqkz40W9JLpowAP2xC-yXqu6fg5zT8"#getpass.getpass("Total de fragmentos")
 
-    if "GOOGLE_API_KEY" not in os.environ:
-        os.environ["GOOGLE_API_KEY"] = getpass.getpass("Total de fragmentos")
+        checkpoint = "gemini-2.0-flash"
+        llm = ChatGoogleGenerativeAI(model=checkpoint)
 
-    checkpoint = "gemini-2.0-flash"
-    llm = ChatGoogleGenerativeAI(model=checkpoint)
+        embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+        embedded_chunks = [embeddings.embed_query(chunk) for chunk in chunks]
+        #embedded_chunks = [embeddings.embed_query([chunk])[0] for chunk in chunks]
 
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-    embedded_chunks = [embeddings.embed_query([chunk])[0] for chunk in chunks]
+        print(f"Ejemplo de embedding: {embedded_chunks[0][:5]}")  # Print the number of embedded chunks
 
-    print(f"Ejemplo de embedding: {embedded_chunks[0][:5]}")  # Print the number of embedded chunks
+        documents = [Document(page_content=chunk) for chunk in chunks]
+        vectorstore = Chroma.from_documents(
+            documents = documents,
+            embedding = embeddings, 
+            persist_directory="./chroma_menu_db"
+        )
 
-    documents = [Document(page_content=chunk) for chunk in chunks]
-    vectorstore = Chroma.from_documents(
-        documents = documents,
-        embedding = embeddings, 
-        persist_directory="./chroma_menu_db"
-    )
+        print("Base de datos vectorial creada exitosamente.")
 
-    print("Base de datos vectorial creada exitosamente.")
+        retriever = vectorstore.as_retriever(search_kwargs={"k": 5})
+        retrieve = RunnableMap({
+            "context": lambda x: retriever.invoke(x["question"]),
+            "question": lambda x: x["question"]
+        })
 
-    retriever = vectorstore.as_retriever(search_kwargs={"k": 5})
-    retrieve = RunnableMap({
-        "context": lambda x: retriever.invoke(x["question"]),
-        "question": lambda x: x["question"]
-    })
+        #Definir el Prompt en Español
+        prompt_template = """Utiliza el siguiente contexto para responder la pregunta.\n
+        Si no sabes la respuesta, simplemente di que no lo sabes.\n\n
+        {context}\n\n
 
-    #Definir el Prompt en Español
-    prompt_template = """Utiliza el siguiente contexto para responder la pregunta.\n
-    Si no sabes la respuesta, simplemente di que no lo sabes.\n\n
-    {context}\n\n
+        Pregunta: {question}\nRespuesta:"""
 
-    Pregunta: {question}\nRespuesta:"""
+        PROMPT = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
 
-    PROMPT = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+        chat_prompt = ChatPromptTemplate.from_messages([
+            ("system", ""), # Crear mensaje del sistema
+            ("user", prompt_template),
+        ])
 
-    chat_prompt = ChatPromptTemplate.from_messages([
-        ("system", ""), # Crear mensaje del sistema
-        ("user", prompt_template),
-    ])
+        output_parser = StrOutputParser()
+        chain = retrieve | chat_prompt | llm | output_parser
 
-    output_parser = StrOutputParser()
-    chain = retrieve | chat_prompt | llm | output_parser
+        # query = "¿Qué incluye el paquete de explorando Groelandia?"
+        # response = chain.invoke({"question": query})
+        # print(response)
 
-    query = "¿Qué incluye el paquete de explorando Groelandia?"
-    response = chain.invoke({"question": query})
-    print(response)
-
-    # query = "¿Qué paquetes turístico ofrece la empresa?"
-    # response = chain.invoke({"question": query})
-    # print(response)
-    query = text
-    response = chain.invoke({"question": query})
-    return response
+        # query = "¿Qué paquetes turístico ofrece la empresa?"
+        # response = chain.invoke({"question": query})
+        # print(response)
+        query = text
+        response = chain.invoke({"question": query})
+        return response
+    except Exception as exception:
+        print(exception)
+        return "error"
